26905be1d739ae56c5df8ca49b319be11e1b145f
aliceinwire <alice.ferrazzi@gmail.com>
2017-05-25 19:55:02 +0000

    test

--- files/stabilize-packages.py
+++ files/stabilize-packages.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 import sys
+import subprocess
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
+packages = sys.argv[1:]
+gentoo_repo = '../gentoo/'
+
+def command(cmd):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    for line in proc.stdout:
+        a=(line.strip('\n'))
+        print(a)
+
+
+for package in packages: 
+    ebuild_location = gentoo_repo + package  
+    ebuild_full = 'ebuild ' + ebuild_location
+    ebuild_manifest = ebuild_full + ' manifest'
+    ebuild_merge = ebuild_full + ' merge '
+
+    command(ebuild_manifest)
+    command(ebuild_merge)
