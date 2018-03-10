687dbfa3b4f2a16e108a12e21cfb54e270d4b0db
Romain Perier <romain.perier@gmail.com>
2018-03-10 18:45:13 +0100

    patch-kernel: Add support for config fragment per architecture
    This adds a new option for the desired target architecture. Then the
    default configuration to be used is found according to the kernel
    version and the architecture. Moreover, the kernel is built out of the
    source directory in order to share the same source between kernel
    builders of the same branch and not collide together during the build.
    
    Signed-off-by: Romain Perier <romain.perier@gmail.com>

--- files/patch-kernel.sh
+++ files/patch-kernel.sh
@@ -7,6 +7,7 @@ Usage: ${0##*/} [-hv] [-k] [KERNEL VERSION]
 Do stuff with FILE and write the result to standard output. With no FILE
 or when FILE is -, read standard input.
 
+    -a          kernel architecture to use for search configuration file.
     -h          display this help and exit
     -k          use the kernel version for search configuration file.
     -v          verbose mode. Can be used multiple times for increased
@@ -16,14 +17,18 @@ EOF
 
 # Initialize our own variables:
 kernel_version=""
+kernel_arch="amd64"
 verbose=0
 
 OPTIND=1
 # Resetting OPTIND is necessary if getopts was used previously in the script.
 # It is a good idea to make OPTIND local if you process options in a function.
 
-while getopts hvk: opt; do
+while getopts a:hvk: opt; do
     case $opt in
+        a)
+            kernel_arch=$OPTARG
+	    ;;
         h)
             show_help
             exit 0
@@ -40,15 +45,26 @@ while getopts hvk: opt; do
 done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 
+kernel_arch_target="x86_64"
+if [ $kernel_arch = "arm" ]; then
+	kernel_arch_target="arm"
+fi
+
 # End of file
 for i in ../linux-patches/*.patch; do 
 	echo "${i}"
 	yes "" | patch -p1 --no-backup-if-mismatch -f -N -s -d linux-*/ < "${i}"; 
 done
+kernel_builddir="$(dirname $(realpath $0))/linux-${kernel_arch}-build"
 cd linux-*/ || exit 1
+test -d $kernel_builddir | mkdir $kernel_builddir
 if [ ! -z "${kernel_version}" ]; then
-	echo "searching configuration in ~/kernel-config/config-${kernel_version}"
-	if [ ! -f ~/kernel-config/config-"${kernel_version}" ]; then
+	version="${kernel_version}-${kernel_arch}"
+	if [ -f ~/kernel-config/config-${version}_defconfig ]; then
+		defconfig=$(cat ~/kernel-config/config-${version}_defconfig)
+		echo "Using defconfig ${defconfig}"
+		make ARCH=${kernel_arch_target} O=${kernel_builddir} ${defconfig}
+	elif [ ! -f ~/kernel-config/config-"${version}" ]; then
 		echo "File not found!"
 		echo "searching configuration in /proc/config.gz"
 		if [ ! -f ~/kernel-config ]; then
@@ -56,12 +72,15 @@ if [ ! -z "${kernel_version}" ]; then
 			zcat /proc/config.gz > .config
 		else
 			echo "using defconfig"
-			make defconfig
+			make ARCH=${kernel_arch_target} O=${kernel_builddir} defconfig
 		fi
+		make mrproper
+		yes "" | make ARCH=${kernel_arch_target} O=${kernel_builddir} oldconfig
 	else
-		echo "Using ~/kernel-config/config-${kernel_version}"
-		cp ~/kernel-config/config-"${kernel_version}" .config
+		echo "Using ~/kernel-config/config-${version}"
+		cp ~/kernel-config/config-"${version}" .config
+		make mrproper
+		yes "" | make ARCH=${kernel_arch_target} O=${kernel_builddir} oldconfig
 	fi
 fi
-yes "" | make oldconfig
 head Makefile
