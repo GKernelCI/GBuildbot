371020da685ccf33b43ccdf30b7dc79df98d8db9
Michael Everitt <m.j.everitt@iee.org>
2018-09-29 22:58:02 +0100

    Amend previous commit to remove build id from qcow img filename.
    Qcow image should be static between builds (ideally) so instead
    of naming it with the worker id *and* the build number, just use
    the worker ID, as there shouldn't ever be more instances of the
    build than there are workers configured.

--- files/qemu_check.py
+++ files/qemu_check.py
@@ -44,13 +44,15 @@ def command(cmd, timeout_sec):
     return work
 
 if arch == 'amd64':
-    vmimage = "/tmp/gentoo-amd64-w" + worker + "-b" + build + ".qcow2"
+    vmimage = "/tmp/gentoo-amd64-w" + worker + ".qcow2"
+    # + "-b" + build
     cmd_qemu = 'qemu-system-x86_64 -m 128M -kernel ' \
         'linux-amd64-build/arch/x86/boot/bzImage' \
         ' -nographic -serial mon:stdio -hda ' + vmimage + \
         ' -append "root=/dev/sda1 console=ttyS0,115200n8 console=tty0"'
 elif arch == 'arm':
-    vmimage = "/tmp/gentoo-arm-w" + worker + "-b" + build + ".qcow2"
+    vmimage = "/tmp/gentoo-arm-w" + worker + ".qcow2"
+    # + "-b" + build + ".qcow2"
     cmd_qemu = 'qemu-system-arm -M vexpress-a9 -smp 2 -m 1G -kernel ' \
         'linux-arm-build/arch/arm/boot/zImage' \
         ' -dtb linux-arm-build/arch/arm/boot/dts/vexpress-v2p-ca9.dtb' \
