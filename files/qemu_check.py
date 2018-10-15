796a33f29e9b248309f2a86f89552adb33bd83cf
Michael Everitt <m.j.everitt@iee.org>
2018-10-15 05:56:25 +0100

    Improve qemu test by creating a copy-on-write clone of test image
    This should prevent clashes from multiple builds trying to access
    the same qemu test image at once.
    
    Signed-off-by: Michael Everitt <m.j.everitt@iee.org>

--- files/qemu_check.py
+++ files/qemu_check.py
@@ -7,12 +7,12 @@ import shelve
 import sys
 
 if len(sys.argv) < 4:
-    print("Usage: qemu_check.py [<arch>] <worker#> <build#>")
+    print("Usage: qemu_check.py <arch> <builder> <build#>")
     sys.exit(1)
 #arch = "amd64" if len(sys.argv) < 4 else sys.argv[1]
 arch   = sys.argv[1]
-worker = sys.argv[2]
-build  = sys.argv[3]
+builder = sys.argv[2]
+build_id  = sys.argv[3]
 
 conf_var = "shelve"
 d = shelve.open(conf_var)
@@ -20,10 +20,13 @@ vmlinuz_list = d["version"]
 d.close()
 
 qemu_timeout = 360
-BaseURIamd64 = 'http://gentoo.osuosl.org/experimental/amd64/openstack/'\
-          'gentoo-openstack-amd64-default-'
-BaseURIarm = ''
 SnapshotDate = 'latest'
+BaseURIamd64 = 'http://gentoo.osuosl.org/experimental/amd64/openstack/'
+amd64img = 'gentoo-openstack-amd64-default-' + SnapshotDate + '.qcow2'
+BaseURIarm32 = ''
+arm32img = ''
+BaseURIarm64 = ''
+arm64img = ''
 
 
 def command(cmd, timeout_sec):
@@ -34,9 +37,8 @@ def command(cmd, timeout_sec):
     try:
         timer.start()
         for line in proc.stdout:
-            a = line.strip()
-            print(a)
-            if 'This is localhost' in str(a):
+            print(line.strip())
+            if 'This is localhost' in str(line.strip()):
                 work = True
                 break
     finally:
@@ -44,43 +46,51 @@ def command(cmd, timeout_sec):
     proc.kill()
     return work
 
+# Set up some variables depending on arch
 if arch == 'amd64':
-    vmimage = "/tmp/gentoo-amd64-w" + worker + ".qcow2"
-    # + "-b" + build
+    ImageURI = BaseURIamd64 + amd64img
+    vmimage_src = amd64img
+    vmimage_dest = "gentoo-amd64-" + builder + "-b" + build_id + ".qcow2"
     cmd_qemu = 'qemu-system-x86_64 -m 128M -kernel ' \
         'linux-amd64-build/arch/x86/boot/bzImage' \
-        ' -nographic -serial mon:stdio -hda ' + vmimage + \
+        ' -nographic -serial mon:stdio -hda /tmp/' + vmimage_dest + \
         ' -append "root=/dev/sda1 console=ttyS0,115200n8 console=tty0"'
 elif arch == 'arm':
-    vmimage = "/tmp/gentoo-arm-w" + worker + ".qcow2"
-    # + "-b" + build + ".qcow2"
+    ImageURI = BaseURIarm + arm32img
+    vmimage_src = arm32img
+    vmimage_dest = "gentoo-arm-" + builder + "-b" + build_id + ".qcow2"
     cmd_qemu = 'qemu-system-arm -M vexpress-a9 -smp 2 -m 1G -kernel ' \
         'linux-arm-build/arch/arm/boot/zImage' \
         ' -dtb linux-arm-build/arch/arm/boot/dts/vexpress-v2p-ca9.dtb' \
-        ' -sd ' + vmimage + ' -nographic -append "console=ttyAMA0,115200' \
+        ' -sd /tmp/' + vmimage_dest + ' -nographic -append "console=ttyAMA0,115200' \
         ' root=/dev/mmcblk0 rootwait"'
 
-if not os.path.isfile(vmimage):
-    if arch == 'amd64':
-        ImageURI = BaseURIamd64 + SnapshotDate + '.qcow2'
-    elif arch == 'arm':
-        ImageURI = BaseURIarm + SnapshotDate + '.qcow2'
-    cmd_wget = 'wget -N ' + ImageURI + ' -O ' + vmimage
+# Check for existing base image, download if needed
+if not os.path.isfile(vmimage_src):
+    cmd_wget = 'wget -Nc ' + ImageURI + ' -P /tmp'
     proc2 = subprocess.Popen(cmd_wget, stdout=subprocess.PIPE, shell=True)
     for line in proc2.stdout:
-        a = line.strip()
-        print(a)
-    if not os.path.isfile(vmimage):
+        print(line.strip())
+    if not os.path.isfile(vmimage_src):
         print("Cannot download file: " + ImageURI)
         sys.exit(1)
 else:
-    print("vmimage present: " + vmimage)
+    print("vmimage present: " + vmimage_src)
 
+# Create snapshot of base image for build
+cmd_clone_qemu_img = 'qemu-img create -f qcow2 -b ' + vmimage_src + ' ' + \
+                     vmimage_dest
+proc2 = subprocess.Popen(cmd_clone_qemu_img, stdout=subprocess.PIPE, shell=True)
+for line in proc2.stdout:
+    print(line.strip())
+
+# Print [imported] list of kernels to build
 print(vmlinuz_list)
 
 if isinstance(vmlinuz_list, str):
     vmlinuz_list = [vmlinuz_list]
 
+# Build the kernels...
 for vmlinuz in vmlinuz_list:
     print(vmlinuz)
     work = command(cmd_qemu, qemu_timeout)
