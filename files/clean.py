19403b3566af50144397477d5bb5288f91f599a2
Michael Everitt <m.j.everitt@iee.org>
2018-10-15 07:23:01 +0100

    Overhaul clean.py, add some debugging info
    Signed-off-by: Michael Everitt <m.j.everitt@iee.org>

--- files/clean.py
+++ files/clean.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import subprocess
+import os
 import shelve
+import subprocess
 
 
 def command(cmd):
@@ -16,15 +17,30 @@ d = shelve.open(conf_var)
 packages = d["version"]
 d.close()
 
-# filter manifest files
+# filter for Manifest files
 packages = [v for v in packages if "Manifest" not in v]
 gentoo_repo = 'gentoo/'
 
+# remove any existing script
+if os.path.isfile('ebuild_unmerge.sh'):
+    os.unlink('ebuild_unmerge.sh')
+
+# write script header shebang
+cmd_echo = 'echo "#!/bin/sh" > ebuild_unmerge.sh'
+command(cmd_echo)
+
+# Do the Thing ...
+# 1) build the ebuild script
 for package in packages:
+    print("Cleaning package: {0}".format(package))
     ebuild_location = gentoo_repo + package
     ebuild_full = "ROOT=kernel_sources/ /usr/bin/ebuild " + ebuild_location
     ebuild_unmerge = ebuild_full + ' unmerge '
-    cmd_echo = 'echo "' + ebuild_unmerge + '" > ebuild_unmerge.sh &&' + \
-               ' chmod +x ebuild_unmerge.sh'
+    print("  {0}".format(ebuild_unmerge))
+    cmd_echo = 'echo "' + ebuild_unmerge + '" >> ebuild_unmerge.sh'
     command(cmd_echo)
-    command('./ebuild_unmerge.sh')
+
+# 2) execute it
+cmd_echo = 'chmod +x ebuild_unmerge.sh'
+command(cmd_echo)
+command('./ebuild_unmerge.sh')
