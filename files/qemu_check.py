d7fa425e26b9f06690cb0dc71d45d7fc78f669be
M. J. Everitt <m.j.everitt@iee.org>
2019-03-10 21:20:33 +0000

    Fix paths for cloning qcow image
    Signed-off-by: M. J. Everitt <m.j.everitt@iee.org>

--- files/qemu_check.py
+++ files/qemu_check.py
@@ -71,20 +71,20 @@ elif arch == 'arm':
         ' root=/dev/mmcblk0 rootwait"'
 
 # Check for existing base image, download if needed
-if not os.path.isfile(vmimage_src):
+if not os.path.isfile('/tmp/' + vmimage_src):
     cmd_wget = 'wget -Nc ' + ImageURI + ' -P /tmp'
     proc2 = subprocess.Popen(cmd_wget, stdout=subprocess.PIPE, shell=True)
     for line in proc2.stdout:
         print(line.strip())
-    if not os.path.isfile(vmimage_src):
+    if not os.path.isfile('/tmp/' + vmimage_src):
         print("Cannot download file: " + ImageURI)
         sys.exit(1)
 else:
     print("vmimage present: " + vmimage_src)
 
 # Create snapshot of base image for build
-cmd_clone_qemu_img = 'qemu-img create -f qcow2 -b ' + vmimage_src + ' ' + \
-                     vmimage_dest
+cmd_clone_qemu_img = 'qemu-img create -f qcow2 -b /tmp/' + vmimage_src + ' ' + \
+                     '/tmp/' + vmimage_dest
 proc2 = subprocess.Popen(cmd_clone_qemu_img, stdout=subprocess.PIPE, shell=True)
 for line in proc2.stdout:
     print(line.strip())
