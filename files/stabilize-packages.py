mjeveritt/Gentoo_kernelCI

Overhaul stabilize-packages script
Signed-off-by: Michael Everitt <m.j.everitt@iee.org>

files/stabilize-packages.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
-import subprocess
 import shelve
+import subprocess
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
@@ -17,30 +18,52 @@ def command(cmd):
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,
                             universal_newlines=True)
     for line in proc.stdout:
-        a = line.strip('')
-        print(a)
+        print(line.strip())
+    return proc.returncode
+
 
 conf_var = "shelve"
 d = shelve.open(conf_var)
 d["version"] = []
-
 versions = []
 
+# write script headers
+with open('ebuild_merge.sh', 'w') as eb1:
+    eb1.write("#!/bin/sh")
+    eb1.write("set -e")
+
+with open('ebuild_manifest.sh', 'w') as eb2:
+    eb2.write("#!/bin/sh")
+    eb2.write("set -e")
+
+
+ebm = open("ebuild_manifest.sh", 'a')
+ebg = open("ebuild_merge.sh", 'a')
 for package in packages:
+    print("Processing: {0}".format(package))
     ebuild_location = gentoo_repo + package
     ebuild_full = 'ROOT=kernel_sources/ /usr/bin/ebuild ' + ebuild_location
-    ebuild_manifest = ebuild_full + ' manifest'
-    ebuild_merge = ebuild_full + ' merge '
+    print("  {0}".format(ebuild_full))
 
-    cmd_emg_manifest = 'echo "' + ebuild_manifest + \
-        '" > ebuild_manifest.sh && chmod +x ebuild_manifest.sh'
-    command(cmd_emg_manifest)
-    cmd_emg_merge = 'echo "' + ebuild_merge + \
-        '" > ebuild_merge.sh && chmod +x ebuild_merge.sh'
-    command(cmd_emg_merge)
-    command('./ebuild_merge.sh')
-    command('./ebuild_manifest.sh')
+    ebm.write(ebuild_full + ' clean manifest')
+    ebg.write(ebuild_full + ' merge ')
+
     versions.append(package)
+ebm.close()
+ebg.close()
+
+os.chmod('ebuild_merge.sh', 0o755)
+os.chmod('ebuild_manifest.sh', 0o755)
+
+result = command('./ebuild_manifest.sh')
+if result > 0:
+    print("Manifest generation failed")
+    sys.exit(1)
+
+result = command('./ebuild_merge.sh')
+if result > 0:
+    print("Emerging failed")
+    sys.exit(1)
 
 d["version"] = versions
 d.close()

GKernelCI/GBuildbot

files/stabilize-packages.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
-import subprocess
 import shelve
+import subprocess
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
@@ -23,24 +24,37 @@ def command(cmd):
 conf_var = "shelve"
 d = shelve.open(conf_var)
 d["version"] = []
-
 versions = []
 
+# write script headers
+with open('ebuild_merge.sh', 'w') as ebuild_merge:
+    ebuild_merge.write("#!/bin/sh\n")
+    ebuild_merge.write("set -e\n")
+
+with open('ebuild_manifest.sh', 'w') as ebuild_manifest:
+    ebuild_manifest.write("#!/bin/sh\n")
+    ebuild_manifest.write("set -e\n")
+
+
+ebuild_manifest = open("ebuild_manifest.sh", 'a')
+ebuild_merge = open("ebuild_merge.sh", 'a')
 for package in packages:
+    # .sort(key=lambda s: [int(u) for u in s.split('.')]):
+    print("Processing: {0}".format(package))
     ebuild_location = gentoo_repo + package
     ebuild_full = 'ROOT=kernel_sources/ /usr/bin/ebuild ' + ebuild_location
-    ebuild_manifest = ebuild_full + ' manifest'
-    ebuild_merge = ebuild_full + ' merge '
+    print("  {0}".format(ebuild_full))
 
-    cmd_emg_manifest = 'echo "' + ebuild_manifest + \
-        '" > ebuild_manifest.sh && chmod +x ebuild_manifest.sh'
-    command(cmd_emg_manifest)
-    cmd_emg_merge = 'echo "' + ebuild_merge + \
-        '" > ebuild_merge.sh && chmod +x ebuild_merge.sh'
-    command(cmd_emg_merge)
-    command('./ebuild_merge.sh')
-    command('./ebuild_manifest.sh')
+    ebuild_manifest.write(ebuild_full + ' clean manifest\n')
+    ebuild_merge.write(ebuild_full + ' install\n')
+
     versions.append(package)
+ebuild_manifest.close()
+ebuild_merge.close()
+
+os.chmod('ebuild_merge.sh', 0o755)
+os.chmod('ebuild_manifest.sh', 0o755)
+
 
 d["version"] = versions
 d.close()