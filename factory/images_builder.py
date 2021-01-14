bf192e2eec286b5e3d8e0a704610ce5ce98f8e82
Alice Ferrazzi <alicef@gentoo.org>
2021-01-15 06:24:55 +0900

    Disable logging eviroment variables on steps logs
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- factory/images_builder.py
+++ factory/images_builder.py
@@ -11,17 +11,20 @@ def download_new_patch_and_build_kernel(version, arch):
                                  descriptionDone='Cleaned enviroment',
                                  name='Clean enviroment',
                                  command=["/bin/bash", "-c", "umask 022; rm -rf *"],
+                                 logEnviron=False,
                                  timeout=2400))
 
     factory.addStep(steps.GitHub(name="Fetching linux-patches",
                                  repourl='https://github.com/gentoo/linux-patches',
                                  mode='incremental',
+                                 logEnviron=False,
                                  workdir="build/linux-patches", branch=version))
     
     factory.addStep(steps.GitHub(name="Fetching Ghelper",
                                  repourl='https://github.com/GKernelCI/Ghelper.git',
                                  mode='incremental',
                                  alwaysUseLatest=True,
+                                 logEnviron=False,
                                  workdir="build/ghelper", branch='master'))
 
     factory.addStep(steps.ShellCommand(name="Looking for new upstream release",
@@ -29,36 +32,43 @@ def download_new_patch_and_build_kernel(version, arch):
                                                 "check-kernelpage.py",
                                                 "--version", version],
                                        workdir="build/ghelper/",
+                                       logEnviron=False,
                                        haltOnFailure=True))
 
     factory.addStep(steps.ShellCommand(name="Patching kernel",
                                        command=["/bin/sh", "patch-kernel.sh",
                                                 "-a", arch, "-k", version],
                                        workdir="build/ghelper/",
+                                       logEnviron=False,
                                        haltOnFailure=True))
 
     factory.addStep(steps.ShellCommand(name="Listing rejected files",
                                        command=["/bin/sh", "find.sh"],
+                                       logEnviron=False,
                                        workdir="build/ghelper/"))
 
     factory.addStep(steps.ShellCommand(name="Building kernel",
                                        command=["/bin/sh", "../build-kernel.sh", arch],
                                        workdir="build/ghelper/linux-" + version + "/",
+                                       logEnviron=False,
                                        haltOnFailure=True))
 
     factory.addStep(steps.ShellCommand(name="Building modules",
                                        command=["/bin/sh", "../build-kernel.sh", arch, "modules"],
                                        workdir="build/ghelper/linux-" + version + "/",
+                                       logEnviron=False,
                                        haltOnFailure=True))
 
     factory.addStep(steps.ShellCommand(name="Move kernel to fileserver",
                                        command=["/bin/sh", "to_fileserver.sh", arch,
                                                 util.Property('buildername'), util.Property('buildnumber')],
+                                       logEnviron=False,
                                        workdir="build/ghelper/", timeout=3600))
     
     factory.addStep(steps.ShellCommand(name="Run Gentoo kernel tests",
                                        command=["/bin/sh", "run_tests.sh", arch,
                                                 util.Property('buildername'), util.Property('buildnumber')],
+                                       logEnviron=False,
                                        workdir="build/ghelper/", timeout=3600))
     return factory
 
@@ -85,17 +95,21 @@ def test_gentoo_sources():
                                  descriptionDone='Cleaned enviroment',
                                  name='Clean enviroment',
                                  command=["/bin/bash", "-c", "umask 022; rm -rf *"],
+                                 logEnviron=False,
                                  timeout=2400))
     factory.addStep(steps.GitHub(name="Fetching repository",
                                  repourl=pull_repourl,
+                                 logEnviron=False,
                                  mode='incremental', workdir="build/gentoo", shallow=50))
     factory.addStep(steps.GitHub(name="Fetching Ghelper",
                                  repourl='https://github.com/GKernelCI/Ghelper.git',
                                  mode='incremental',
+                                 logEnviron=False,
                                  alwaysUseLatest=True,
                                  workdir="build/ghelper", branch='master'))
     factory.addStep(steps.ShellCommand(name="Stabilizing package",
                                        command=filterFiles,
+                                       logEnviron=False,
                                        workdir="build/ghelper/"))
     return factory
 
