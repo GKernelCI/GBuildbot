810451046d090d7ab01f08424234e4f353a59bb6
Alice Ferrazzi <alicef@gentoo.org>
2021-01-21 21:06:48 +0900

    Remove 5.9 and add 5.11
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>
    Acked-by: Michael Everitt <gentoo@veremit.xyz>

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Define branch to build
-branches_list = ['5.10', '5.9', '5.4', '4.19',
+branches_list = ['5.11', '5.10', '5.4', '4.19',
                  '4.14', '4.9', '4.4']
 
