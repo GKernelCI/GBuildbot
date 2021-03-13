138d3defa116f6b153ba61f3c1a646e19d9861ba
Alice Ferrazzi <alicef@gentoo.org>
2021-03-13 19:22:07 +0900

    Use different path for each gitpoller
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- sources.py
+++ sources.py
@@ -18,17 +18,20 @@ sources.append(changes.GitPoller(
     repourl='https://github.com/gentoo/gentoo.git',
     branches=True,
     category="gentoo-pull",
+    workdir="gentoo-repository",
     pollinterval=300))
 
 sources.append(changes.GitPoller(
     repourl='https://github.com/GKernelCI/linux-patches.git',
     only_tags=True,
     category="gentoo-tags-git",
+    workdir="linux-patches-tags",
     pollinterval=300))
 
 sources.append(changes.GitPoller(
     repourl='https://github.com/GKernelCI/linux-patches.git',
     branches=True,
     category="gentoo-git",
+    workdir="linux-patches-branches",
     pollinterval=300))
 
